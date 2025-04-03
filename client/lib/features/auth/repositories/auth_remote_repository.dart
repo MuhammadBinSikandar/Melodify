ed1afc74a534d82ebb531cedfc150ff84674db8c
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-04-03 14:24:42 +0500

    Setup the auth with the UserModel and AppFailure

--- client/lib/features/auth/repositories/auth_remote_repository.dart
+++ client/lib/features/auth/repositories/auth_remote_repository.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:client/core/constants/server_constant.dart';
+import 'package:client/core/failure/failure.dart';
+import 'package:client/features/auth/model/user_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:fpdart/fpdart.dart';
 
 class AuthRemoteRepository {
   final String baseUrl = "http://192.168.100.11:8000";
-  Future<Either<Failure, Map<String, dynamic>>> signup({
+  Future<Either<AppFailure, UserModel>> signup({
     required String email,
     required String password,
     required String name,
   }) async {
     try {
-      final Uri url = Uri.parse('$baseUrl/auth/signup');
+      final Uri url = Uri.parse('${ServerConstant.serverURL}/auth/signup');
 
       final Map<String, dynamic> requestBody = {
         'email': email,
@@ -26,32 +29,31 @@ class AuthRemoteRepository {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
         },
-        body: jsonEncode(requestBody),
+        body: jsonEncode({'email': email, 'password': password, 'name': name}),
       );
+      final resBodyMap = jsonDecode(response.body) as Map<String, dynamic>;
 
       if (response.statusCode != 201) {
-        return Left(response.body);
+        return Left(AppFailure(resBodyMap['detail']));
       }
       print('✅ Signup successful');
-      final user = jsonDecode(response.body) as Map<String, dynamic>;
-      return Right(user);
+      return Right(UserModel.fromMap(resBodyMap));
     } catch (e) {
-      return Left(e.toString());
+      return Left(AppFailure(e.toString()));
     }
   }
 
-  Future<void> login({required String email, required String password}) async {
+  Future<Either<AppFailure, UserModel>> login({
+    required String email,
+    required String password,
+  }) async {
     try {
-      final Uri url = Uri.parse('$baseUrl/auth/login');
+      final Uri url = Uri.parse('${ServerConstant.serverURL}/auth/login');
 
       final Map<String, dynamic> requestBody = {
         'email': email,
         'password': password,
       };
-
-      print('Attempting login...');
-      print('Request URL: $url');
-      print('Request Body: ${jsonEncode(requestBody)}');
 
       final response = await http.post(
         url,
@@ -61,28 +63,14 @@ class AuthRemoteRepository {
         },
         body: jsonEncode(requestBody),
       );
-
-      print('Response Status Code: ${response.statusCode}');
-      print('Response Body: ${response.body}');
-      print('Response Headers: ${response.headers}');
-
-      if (response.statusCode == 200) {
-        print('✅ Login successful');
-      } else {
-        print('❌ Login failed with status ${response.statusCode}');
-        print('Error Details: ${response.body}');
+      final resBodyMap = jsonDecode(response.body) as Map<String, dynamic>;
+      if (response.statusCode != 200) {
+        return Left(AppFailure(resBodyMap['detail']));
       }
-    } on SocketException catch (e) {
-      print('🚨 Socket Exception: Unable to connect to the server');
-      print('Error details: $e');
-    } on TimeoutException catch (e) {
-      print('⏳ Timeout Error: Connection timed out');
-      print('Error details: $e');
-    } on HttpException catch (e) {
-      print('📡 HTTP Exception: ${e.message}');
+      print('✅ Login successful');
+      return Right(UserModel.fromMap(resBodyMap));
     } catch (e) {
-      print('❗ Unexpected error during login');
-      print('Error details: $e');
+      return Left(AppFailure(e.toString()));
     }
   }
 }
