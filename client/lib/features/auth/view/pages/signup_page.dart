cc65a63d1ee879155ebb97b914a43c68429cdf93
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-04-03 16:20:38 +0500

    Refactor SignupPage to use Riverpod for state management and add loading indicator

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,19 +1,21 @@
 import 'package:client/core/theme/app_pallete.dart';
-import 'package:client/features/auth/repositories/auth_remote_repository.dart';
+import 'package:client/core/utils.dart';
+import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/auth/view/pages/login_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
+import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:fpdart/fpdart.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignupPage extends StatefulWidget {
+class SignupPage extends ConsumerStatefulWidget {
   const SignupPage({super.key});
 
   @override
-  State<SignupPage> createState() => _SignupPageState();
+  ConsumerState<SignupPage> createState() => _SignupPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
+class _SignupPageState extends ConsumerState<SignupPage> {
   final nameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -30,84 +32,112 @@ class _SignupPageState extends State<SignupPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authViewmodelProvider)?.isLoading == true;
+    ref.listen(authViewmodelProvider, (_, next) {
+      next?.when(
+        data: (data) {
+          showSnackBar(context, 'Account created successfully! Please login.');
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+          );
+        },
+        error: (error, st) {
+          showSnackBar(context, error.toString());
+        },
+        loading: () {},
+      );
+    });
+
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        // Add this to make the content scrollable
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ), // Add some top padding for better spacing
-                const Text(
-                  'Sign Up.',
-                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 30),
-                CustomField(hintText: 'Name', controller: nameController),
-                const SizedBox(height: 15),
-                CustomField(hintText: 'Email', controller: emailController),
-                const SizedBox(height: 15),
-                CustomField(
-                  hintText: 'Password',
-                  controller: passwordController,
-                  isObscureText: true,
-                ),
-                const SizedBox(height: 20),
-                AuthGradientButton(
-                  buttonText: 'Sign Up',
-                  OnTap: () async {
-                    final res = await AuthRemoteRepository().signup(
-                      email: emailController.text,
-                      password: passwordController.text,
-                      name: nameController.text,
-                    );
-                    final val = switch (res) {
-                      Left(value: final l) => l,
-                      Right(value: final r) => r.toString(),
-                    };
-                    print(val);
-                  },
-                ),
-                const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginPage(),
-                      ),
-                    );
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      text: "Already have an account? ",
-                      style: Theme.of(context).textTheme.titleMedium,
-                      children: const [
-                        TextSpan(
-                          text: "Sign In",
+      body:
+          isLoading
+              ? const Loader()
+              : SingleChildScrollView(
+                // Add this to make the content scrollable
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 50,
+                        ), // Add some top padding for better spacing
+                        const Text(
+                          'Sign Up.',
                           style: TextStyle(
-                            color: Pallete.gradient2,
+                            fontSize: 50,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
+                        const SizedBox(height: 30),
+                        CustomField(
+                          hintText: 'Name',
+                          controller: nameController,
+                        ),
+                        const SizedBox(height: 15),
+                        CustomField(
+                          hintText: 'Email',
+                          controller: emailController,
+                        ),
+                        const SizedBox(height: 15),
+                        CustomField(
+                          hintText: 'Password',
+                          controller: passwordController,
+                          isObscureText: true,
+                        ),
+                        const SizedBox(height: 20),
+                        AuthGradientButton(
+                          buttonText: 'Sign Up',
+                          OnTap: () async {
+                            if (formKey.currentState!.validate()) {
+                              await ref
+                                  .read(authViewmodelProvider.notifier)
+                                  .signUpUser(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                    name: nameController.text,
+                                  );
+                            }
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginPage(),
+                              ),
+                            );
+                          },
+                          child: RichText(
+                            text: TextSpan(
+                              text: "Already have an account? ",
+                              style: Theme.of(context).textTheme.titleMedium,
+                              children: const [
+                                TextSpan(
+                                  text: "Sign In",
+                                  style: TextStyle(
+                                    color: Pallete.gradient2,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ), // Add bottom padding to ensure scrolling space
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 50,
-                ), // Add bottom padding to ensure scrolling space
-              ],
-            ),
-          ),
-        ),
-      ),
+              ),
     );
   }
 }
