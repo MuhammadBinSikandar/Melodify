8a8fea80f11f987b65b3375ca4c282ac856f3812
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-04-10 19:45:08 +0500

    Update server URL in ServerConstant to new IP address

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -6,5 +6,6 @@ class ServerConstant {
   //         ? 'http:10.0.2.2:8000'
   //         : 'http://192.168.100.148:8000';
   //
-  static const String serverURL = 'http://192.168.100.148:8000';
+  static const String serverURL = 'http://192.168.37.97:8000';
+  // static const String serverURL = 'http://192.168.100.148:8000';
 }
