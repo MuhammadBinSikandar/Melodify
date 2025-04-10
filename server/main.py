46b81938c14710f089ff727eac06f86749b16212
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-04-10 19:47:17 +0500

    Add CORS middleware to FastAPI application for enhanced cross-origin resource sharing

--- server/main.py
+++ server/main.py
@@ -5,6 +5,13 @@ from database import engine
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # replace * with frontend origin in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router, prefix='/auth')
 
 Base.metadata.create_all(engine) 
