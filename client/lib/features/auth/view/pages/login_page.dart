1c3d429dc12f1eaff392c46274d4749441f658b4
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-04-10 19:46:25 +0500

    Refactor LoginPage to navigate to HomePage upon successful login and improve loading state management

--- client/lib/features/auth/view/pages/login_page.dart
+++ client/lib/features/auth/view/pages/login_page.dart
@@ -1,14 +1,13 @@
 import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/loader.dart';
-import 'package:client/features/auth/repositories/auth_remote_repository.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
 import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
+import 'package:client/features/home/view/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:fpdart/fpdart.dart';
 
 class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
@@ -31,15 +30,17 @@ class _LoginPageState extends ConsumerState<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLoading = ref.watch(authViewmodelProvider)?.isLoading == true;
+    final isLoading = ref.watch(
+      authViewmodelProvider.select((val) => val?.isLoading == true),
+    );
     ref.listen(authViewmodelProvider, (_, next) {
       next?.when(
         data: (data) {
           // Move to homepage or dashboard
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => const LoginPage()),
-          // );
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
         },
         error: (error, st) {
           showSnackBar(context, error.toString());
@@ -82,15 +83,16 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                       AuthGradientButton(
                         buttonText: 'Sign In',
                         OnTap: () async {
-                          final res = await AuthRemoteRepository().login(
-                            email: emailController.text,
-                            password: passwordController.text,
-                          );
-                          final val = switch (res) {
-                            Left(value: final l) => l,
-                            Right(value: final r) => r,
-                          };
-                          print(val);
+                          if (formKey.currentState!.validate()) {
+                            await ref
+                                .read(authViewmodelProvider.notifier)
+                                .loginUser(
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                );
+                          } else {
+                            showSnackBar(context, 'Please fill all fields');
+                          }
                         },
                       ),
                       const SizedBox(height: 20),
