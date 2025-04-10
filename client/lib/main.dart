0a9c14992a3194528c396b151d01aea201637ca3
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-04-10 19:45:03 +0500

    Refactor main.dart to include current user state management and update app title to 'Melodify'

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,25 +1,50 @@
+import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:client/core/theme/theme.dart';
+import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:client/features/auth/view/pages/splash_screen.dart';
 import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
+import 'package:client/features/home/view/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final container = ProviderContainer();
-  container.read(authViewmodelProvider.notifier).initSharedPreferences();
+  await container.read(authViewmodelProvider.notifier).initSharedPreferences();
+  await container.read(authViewmodelProvider.notifier).getData();
   runApp(UncontrolledProviderScope(container: container, child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   final container = ProviderContainer();
+//   final notifier = container.read(authViewmodelProvider.notifier);
+//   await notifier.initSharedPreferences();
+//   final userModel = await notifier.getData();
+//   print(userModel);
+//   runApp(UncontrolledProviderScope(container: container, child: const MyApp()));
+// }
+
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserNotifierProvider);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Melodify',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
-      home: const SplashScreen(),
+      home:
+          currentUser == null
+              ? const SplashScreen()
+              : const Scaffold(
+                body: Center(
+                  child: Text(
+                    'Welcome to Melodify!',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ),
     );
   }
 }
