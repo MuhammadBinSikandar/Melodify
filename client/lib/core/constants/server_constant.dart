ae83bd1be63851f7e3e7ea9691833de28214212f
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-13 08:57:33 +0500

    Update serverURL in ServerConstant to the correct IP address

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,11 +1,3 @@
-import 'dart:io';
-
 class ServerConstant {
-  // static String serverURL =
-  //     Platform.isAndroid
-  //         ? 'http:10.0.2.2:8000'
-  //         : 'http://192.168.100.148:8000';
-  //
-  static const String serverURL = 'http://192.168.37.97:8000';
-  // static const String serverURL = 'http://192.168.100.148:8000';
+  static const String serverURL = 'http://192.168.100.96:8000';
 }
