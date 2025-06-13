5e9ebc3678b85392283a7f8c18c94f497d825b86
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-13 08:57:38 +0500

    Update baseUrl in AuthRemoteRepository to the correct server address

--- client/lib/features/auth/repositories/auth_remote_repository.dart
+++ client/lib/features/auth/repositories/auth_remote_repository.dart
@@ -16,7 +16,7 @@ AuthRemoteRepository authRemoteRepository(AuthRemoteRepositoryRef ref) {
 }
 
 class AuthRemoteRepository {
-  final String baseUrl = "http://192.168.100.11:8000";
+  final String baseUrl = "http://192.168.100.96:8000";
   Future<Either<AppFailure, UserModel>> signup({
     required String email,
     required String password,
