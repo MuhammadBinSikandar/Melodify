424c8a0f15cb3768872870a17ec0cd6bd6dfa78c
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-14 15:55:40 +0500

    Refactor audio and image file picking functions to ensure non-empty file selection and improve error handling

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -1,7 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
 
 void showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
     ..showSnackBar(SnackBar(content: Text(content.toString())));
+}
+
+// Future<File?> pickAudio() async {
+//   try {
+//     final filePickerRes = await FilePicker.platform.pickFiles(
+//       type: FileType.audio,
+//     );
+//     if (filePickerRes != null) {
+//       return File(filePickerRes!.files.first.xFile.path);
+//     }
+//     return null;
+//   } catch (e) {
+//     return null; // Handle the error gracefully
+//   }
+// }
+
+// Future<File?> pickImage() async {
+//   try {
+//     final filePickerRes = await FilePicker.platform.pickFiles(
+//       type: FileType.image,
+//     );
+//     if (filePickerRes != null) {
+//       return File(filePickerRes!.files.first.xFile.path);
+//     }
+//     return null;
+//   } catch (e) {
+//     return null; // Handle the error gracefully
+//   }
+// }
+
+Future<File?> pickAudio() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
+    if (filePickerRes != null && filePickerRes.files.isNotEmpty) {
+      final path = filePickerRes.files.first.path;
+      if (path != null) {
+        return File(path);
+      }
+    }
+    return null;
+  } catch (e) {
+    return null; // Handle the error gracefully
+  }
+}
+
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+    );
+    if (filePickerRes != null && filePickerRes.files.isNotEmpty) {
+      final path = filePickerRes.files.first.path;
+      if (path != null) {
+        return File(path);
+      }
+    }
+    return null;
+  } catch (e) {
+    return null; // Handle the error gracefully
+  }
 }
