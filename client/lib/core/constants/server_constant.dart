2f9a32e5eb442d09eefef82501866031bac1b38f
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-15 12:51:59 +0500

    Update serverURL to use port 8080 for server connection

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,4 +1,4 @@
 class ServerConstant {
-  static const String serverURL = 'http://192.168.100.96:8000';
+  static const String serverURL = 'http://192.168.100.96:8080';
   // static const String serverURL = 'http://10.113.73.153:8000';
 }
