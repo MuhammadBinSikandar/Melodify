93e722165778639ab2e51850aead5a0a83629a21
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-15 12:53:00 +0500

    Add Song model to define the structure of the songs table

---
+++ server/models/song.py
@@ -0,0 +1,12 @@
+from sqlalchemy import VARCHAR, TEXT, Column
+from models.base import Base
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(TEXT, primary_key=True)
+    song_url = Column(TEXT, nullable=False)
+    thumbnail_url = Column(TEXT, nullable=False)
+    artist = Column(TEXT)
+    song_name = Column(VARCHAR(100))
+    hex_code = Column(VARCHAR(6))
