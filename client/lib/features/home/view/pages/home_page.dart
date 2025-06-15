0ce9f6653600852c0a3840cd13d885f8d12d9808
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-15 12:52:30 +0500

    Refactor Homepage to use ConsumerStatefulWidget and implement bottom navigation

--- client/lib/features/home/view/pages/home_page.dart
+++ client/lib/features/home/view/pages/home_page.dart
@@ -1,14 +1,59 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/features/home/view/pages/library_page.dart';
+import 'package:client/features/home/view/pages/songs_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/providers/current_user_notifier.dart';
 
-class Homepage extends ConsumerWidget {
+class Homepage extends ConsumerStatefulWidget {
   const Homepage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends ConsumerState<Homepage> {
+  int selectedIndex = 0;
+
+  final pages = const [SongsPage(), LibraryPage()];
+
+  @override
+  Widget build(BuildContext context) {
     final user = ref.watch(currentUserNotifierProvider);
-    print('Current User: $user');
-    return Scaffold(appBar: AppBar(title: const Text('Home Page')));
+    return Scaffold(
+      body: pages[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              selectedIndex == 0
+                  ? 'assets/images/home_filled.png'
+                  : 'assets/images/home_unfilled.png',
+              color:
+                  selectedIndex == 0
+                      ? Pallete.whiteColor
+                      : Pallete.inactiveBottomBarItemColor,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/library.png',
+              color:
+                  selectedIndex == 1
+                      ? Pallete.whiteColor
+                      : Pallete.inactiveBottomBarItemColor,
+            ),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
   }
 }
