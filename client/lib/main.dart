3d4c3d8cc67542790f242a9b30d41f03fc88a85d
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-15 12:51:37 +0500

    Add new images and update main.dart to use Homepage instead of UploadSongPage

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 // import 'package:client/features/auth/view/pages/splash_screen.dart';
 import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
+import 'package:client/features/home/view/pages/home_page.dart';
 // import 'package:client/features/home/view/pages/home_page.dart';
 import 'package:client/features/home/view/pages/upload_song_page.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +26,7 @@ class MyApp extends ConsumerWidget {
       title: 'Melodify',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
-      home: currentUser == null ? const SignupPage() : const UploadSongPage(),
+      home: currentUser == null ? const SignupPage() : const Homepage(),
     );
   }
 }
