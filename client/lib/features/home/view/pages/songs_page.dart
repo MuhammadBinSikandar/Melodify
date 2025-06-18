84f26ce5cb9edc64c065586312b83c631acadbc6
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-18 07:23:47 +0500

    Enhance SongsPage with recently played songs display and improved song selection UI

--- client/lib/features/home/view/pages/songs_page.dart
+++ client/lib/features/home/view/pages/songs_page.dart
@@ -1,17 +1,102 @@
+import 'dart:math';
+
+import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class SongsPage extends ConsumerWidget {
   const SongsPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SafeArea(
+    final recentlyPlayedSongs =
+        ref.watch(homeViewModelProvider.notifier).getRecentlyPlayedSongs();
+    final currentSong = ref.watch(currentSongNotifierProvider);
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      decoration:
+          currentSong == null
+              ? null
+              : BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    hexToColor(currentSong.hex_code),
+                    Pallete.transparentColor,
+                  ],
+                  stops: const [0.0, 0.2],
+                ),
+              ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 36),
+            child: SizedBox(
+              height: 280,
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 3,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                ),
+                itemCount: recentlyPlayedSongs.length,
+                itemBuilder: (context, index) {
+                  final song = recentlyPlayedSongs[index];
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Pallete.borderColor,
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            ref
+                                .read(currentSongNotifierProvider.notifier)
+                                .updateSong(song);
+                          },
+                          child: Container(
+                            width: 56,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(song.thumbnail_url),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(4.0),
+                                bottomLeft: Radius.circular(4.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Flexible(
+                          child: Text(
+                            song.song_name,
+                            style: const TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w700,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            maxLines: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
           Padding(
             padding: EdgeInsets.all(8.0),
             child: Text(
@@ -30,35 +115,56 @@ class SongsPage extends ConsumerWidget {
                       itemCount: songs.length,
                       itemBuilder: (context, index) {
                         final song = songs[index];
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                width: 180,
-                                height: 80,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(song.thumbnail_url),
-                                    fit: BoxFit.cover,
-                                  ),
-                                  borderRadius: BorderRadius.circular(7.0),
-                                ),
-                              ),
-                              const SizedBox(height: 5.0),
-                              SizedBox(
-                                width: 180,
-                                child: Text(
-                                  song.song_name,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w700,
-                                    overflow: TextOverflow.ellipsis,
+                        return GestureDetector(
+                          onTap: () {
+                            ref
+                                .read(currentSongNotifierProvider.notifier)
+                                .updateSong(song);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 180,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(song.thumbnail_url),
+                                      fit: BoxFit.cover,
+                                    ),
+                                    borderRadius: BorderRadius.circular(7.0),
                                   ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(height: 5.0),
+                                SizedBox(
+                                  width: 180,
+                                  child: Text(
+                                    song.song_name,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    maxLines: 1,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 180,
+                                  child: Text(
+                                    song.artist,
+                                    style: TextStyle(
+                                      color: Pallete.subtitleText,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    maxLines: 1,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         );
                       },
