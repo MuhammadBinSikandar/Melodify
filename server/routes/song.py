68ebd198f55d55af821b70f62d50e4312a35976d
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-18 07:24:34 +0500

    Implement favorite song functionality with add/remove logic and list favorites endpoint

--- server/routes/song.py
+++ server/routes/song.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import Session
 from database import get_db
 from middleware.auth_middleware import auth_middleware
 from models.song import Song
+from pydantic_schemas.favotite_song import FavoriteSong
+from models.favorite import Favorite
+from sqlalchemy.orm import joinedload
 
 router = APIRouter()
 
@@ -45,3 +48,33 @@ def upload_song(song: UploadFile= File(...),
 def list_songs(db: Session = Depends(get_db), auth_details= Depends(auth_middleware)):
     songs = db.query(Song).all()
     return songs    
+
+@router.post('/favorite')
+def favorite_song(song: FavoriteSong, 
+                  db: Session=Depends(get_db), 
+                  auth_details=Depends(auth_middleware)):
+    # song is already favorited by the user
+    user_id = auth_details['uid']
+
+    fav_song = db.query(Favorite).filter(Favorite.song_id == song.song_id, Favorite.user_id == user_id).first()
+
+    if fav_song:
+        db.delete(fav_song)
+        db.commit()
+        return {'message': False}
+    else:
+        new_fav = Favorite(id=str(uuid.uuid4()), song_id=song.song_id, user_id=user_id)
+        db.add(new_fav)
+        db.commit()
+        return {'message': True}
+    
+
+@router.get('/list/favorites')
+def list_fav_songs(db: Session=Depends(get_db), 
+               auth_details=Depends(auth_middleware)):
+    user_id = auth_details['uid']
+    fav_songs = db.query(Favorite).filter(Favorite.user_id == user_id).options(
+        joinedload(Favorite.song),
+    ).all()
+    
+    return fav_songs
