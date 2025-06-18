2097e5264fccae6f420b24f126707aee97c7eedc
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-18 07:23:08 +0500

    Refactor SplashScreen to use ConsumerWidget and simplify navigation logic

--- client/lib/features/auth/view/pages/splash_screen.dart
+++ client/lib/features/auth/view/pages/splash_screen.dart
@@ -1,38 +1,38 @@
-// splash_screen.dart
+import 'package:client/core/providers/current_user_notifier.dart';
+import 'package:client/features/auth/view/pages/signup_page.dart';
+import 'package:client/features/home/view/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:client/features/auth/view/pages/signup_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserNotifierProvider);
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    // Redirect to signup page after 3 seconds
+    // Schedule navigation after a delay
     Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const SignupPage()),
-      );
+      if (currentUser == null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const SignupPage()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const Homepage()),
+        );
+      }
     });
-  }
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Spotify style
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Spotify logo from network
             Image.asset('assets/images/spotify.png', width: 300, height: 300),
             const SizedBox(height: 30),
             const CircularProgressIndicator(color: Colors.green),
