ea515c89fb7a16b612d140b5413765868fe49b93
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-06-19 05:55:43 +0500

    Update serverURL in ServerConstant to correct address

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,4 +1,4 @@
 class ServerConstant {
-  static const String serverURL = 'http://192.168.168.97:8080';
+  static const String serverURL = 'http://192.168.100.96:8080';
   // static const String serverURL = 'http://10.113.88.127:8080';
 }
